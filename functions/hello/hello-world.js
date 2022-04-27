4aa57b408bea1ac168f563e4032fa197503ffcf5
Fernando Ticona <thefersh24@gmail.com>
2022-04-26 20:31:18 -0400

    twilio serverless function 'hello world'

---
+++ functions/hello/hello-world.js
@@ -0,0 +1,14 @@
+import {
+  Context,
+  ServerlessCallback,
+} from '@twilio-labs/serverless-runtime-types/types';
+
+/**
+ * 
+ * @param {Context} context 
+ * @param {*} event 
+ * @param {ServerlessCallback} callback 
+ */
+exports.handler = function (context, event, callback) {
+  callback(null, JSON.stringify({data: 'Hello world!'}));
+};
