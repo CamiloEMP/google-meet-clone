695f38b18bc456fb37c9bb71ba4d7a092d629243
Fernando Ticona <thefersh24@gmail.com>
2022-04-28 11:11:45 -0400

    responsive design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,19 +31,19 @@ function Navbar(): JSX.Element {
 }
 
 export const Home = () => {
-  const [over, setOver] = useState(true)
+  const [over, setOver] = useState(false)
   const [action, setAction] = useState<AuthActions>('login')
 
   return (
     <>
       <div className="flex flex-col w-full h-full">
         <Navbar />
-        <main className="flex-1 container mx-auto flex h-full w-full items-center">
-          <div className="w-2/4">
-            <h1 className="text-4xl md:text-5xl font-semibold mb-4">
+        <main className="flex-1 container mx-auto flex h-full w-full items-center flex-col-reverse md:flex-row gap-4 md:gap-0">
+          <div className="w-full md:w-2/4 px-4 md:px-0 flex-1 md:flex-auto">
+            <h1 className="text-4xl md:text-5xl font-semibold mb-4 text-center md:text-left">
               Reunete con tu equipo, familiares y tus amigos
             </h1>
-            <div className="flex gap-2 w-fit items-center">
+            <div className="flex gap-2 w-fit items-center mx-auto md:mx-0">
               <button
                 onClick={() => {
                   setOver(true)
@@ -65,7 +65,7 @@ export const Home = () => {
               </button>
             </div>
           </div>
-          <div className="w-2/4">
+          <div className="w-full md:w-2/4 px-4 md:px-0">
             <img
               src="https://www.gstatic.com/meet/user_edu_brady_bunch_light_81fa864771e5c1dd6c75abe020c61345.svg"
               alt="image home"
