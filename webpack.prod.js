f8c0e8ecdfc8abac6eedf6ec0897b48f4e157362
Fernando Ticona <thefersh24@gmail.com>
2022-04-27 20:00:41 -0400

    webpack-bundle-analizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const baseConfig = require('./webpack.config')
 const { merge } = require('webpack-merge')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = merge(baseConfig, {
   output: {
-    filename: '[fullhash].js'
+    chunkFilename: '[name].[fullhash:8].bundle.js',
+    filename: '[name].[fullhash:8].bundle.js',
+    publicPath: '/'
   },
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    ...(process.env.NODE_ENV === 'development'
+      ? [new BundleAnalyzerPlugin()]
+      : [])
+  ]
 })
