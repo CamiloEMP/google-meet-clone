f1f38c8caad675ebf02a53c7d5cbefe90165575d
Fernando Ticona <thefersh24@gmail.com>
2022-04-29 09:08:34 -0400

    Navbar Auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,6 @@ import { useSession } from '../hooks/useSession'
 
 export const NavBar = () => {
   const { isAuth, user } = useSession()
-  console.log({ user, isAuth })
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -18,15 +17,7 @@ export const NavBar = () => {
             <MeetIcon />
           </span>
         </Link>
-        {isAuth && (
-          <button className="select-none">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://randomuser.me/api/portraits/women/8.jpg"
-              alt="Rounded avatar"
-            />
-          </button>
-        )}
+        {isAuth && <button className="select-none">{user?.email}</button>}
       </div>
     </nav>
   )
