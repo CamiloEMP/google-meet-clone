ab774d9c103eb47fb5166ef8ea3a5355a764626f
Fernando Ticona <thefersh24@gmail.com>
2022-05-05 20:10:03 -0400

    renovacion de index

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { Dashboard } from './pages/Dashboard'
 import { Home } from './pages/Home'
 import { MeetConfig } from './pages/MeetConfig'
 import { Room } from './pages/room'
 import { Profile } from './pages/Profile'
-import { useSession } from './hooks/useSession'
 import { Layout } from './container/Layout'
 import { AuthSign } from './components/auth'
 
 export const App = () => {
-  const { isAuth } = useSession()
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={isAuth ? <Dashboard /> : <Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path=":room" element={false ? <Room /> : <MeetConfig />} />
       </Routes>
